521ff8ab328c1c7ede59bf3a160cfea257d1e237
IlseBM8 <ilse_banos@ciencias.unam.mx>
2025-04-20 23:22:58 -0600

    el ejercicio 2 tiene errores

---
+++ practica8/ejercicio1.asm
@@ -0,0 +1,77 @@
+	.data
+a:	.word 4
+b:	.word 5
+msj: .asciiz "Ingresa un valor para m: "
+	.text
+.globl main
+main:	
+	#preaumbulo main
+	addi 	$sp $sp -8 # se crean 12 espacios 
+	sw   	$a0, 0($sp) #guarda los valores3
+	sw   	$ra, 4($sp) #guarda la direccion para volver
+    	
+	#invocacion de mist_1
+	lw 	$a0, a #guarda a en a0
+	lw 	$a1, b #guarda b en a1
+	jal mist_1
+	
+	#retorno de mist_1
+	move $t0, $v0 #s1 es para guardados tras llamada y v0 para retornos de subrutinas
+	
+	#conclusion main
+	lw $a0, 0($sp) #guarda la direccion para volver
+	lw $ra, 4($sp)
+	addi $sp $sp, 8
+	move $a0, $t0   # mover el resultado a $a0
+	#fin del programa
+	li $v0, 10     # código de salida del sistema
+	syscall        # terminar el programa correctamente
+
+#mist_1 recibe como argumentos $a0 y $a1
+mist_1: 
+	#preambulo mist
+	addi $sp $sp -12 # se crean 12 espacios 
+	sw   $ra, 8($sp) #guarda la direccion para volver
+    	sw   $s0, 4($sp) #guarda los valores 
+    	sw   $a1, 0($sp) #guarda los valores
+    	
+	move	$s0, $a0 #s0=a0
+	move	$t0, $a1 #t0=a1
+	li	$t1, 1 #t1=1
+	
+loop_1: beqz $s0, end_1 #if s0==0 then end_1,	
+	#invocacion de mist_0
+	move	$a0, $t0 #pasa el argumento $a0
+	move	$a1, $t1 #pasa el argumento $a1
+	jal mist_0
+	
+	#retorno de mist_0
+	move	$t1, $v0 #pasa el argumento $a0
+	
+	subi	$s0, $s0, 1 #pasa el argumento $a1
+	j loop_1
+
+end_1: 
+	#conclusion mist_1
+	move	$v0, $t1 #se retorna el resultado en v0
+	lw   	$a1, 0($sp) # recupera los valores iniciales
+	lw   	$s0, 4($sp) # recupera los valores iniciales
+	lw   	$ra, 8($sp) # recupera ra 
+	addi 	$sp, $sp, 12 # destruye la pila
+	jr $ra
+	
+#mist_o recibe como argumentos $a0 y $a1
+mist_0: 
+	#preambulo mist_0
+	addi 	$sp $sp -4 # se crean 12 espacios 
+	sw   	$ra, 0($sp) #guarda la direccion para volver
+
+	mult	$a0, $a1
+	mflo	$v0 #se retorna el resultado en v0
+	
+	#conclusion mist_0
+	lw   $ra, 0($sp) # recupera ra 
+	addi $sp, $sp, 4
+	#regresa a mist_1
+	jr $ra
+	
---
+++ practica8/ejercicio2.asm
@@ -0,0 +1,71 @@
+	.data
+n:	.word 2
+k:	.word 1
+mensaje: .asciiz "El coeficiente binomial es : "
+	.text
+	
+main:
+	#prepara los valores para trabajar
+	lw 	$a0, n #guarda n
+	lw 	$a1, k #guarda 
+	#invoca a coef_bin para obtener el coeficniente binomail
+	jal coef_bin
+	move $s0, $v0  # Guardar resultado en $s0
+	
+	li   $v0, 4         # Syscall para imprimir cadena
+    	la   $a0, mensaje   # Cargar dirección del mensaje
+    	syscall
+    	move $a0, $s0 
+	li   $v0, 1         # Syscall para imprimir entero
+    	syscall
+	j exit	
+
+	
+coef_bin:
+	# PILA
+	addi $sp $sp -12 # se crean 12 espacios 
+	sw   $ra, 0($sp) #guarda la direccion para volver
+    	sw   $a0, 4($sp) #guarda los valores 
+    	sw   $a1, 8($sp) #guarda los valores
+	
+	# CASOS BASE
+	beqz $a1, caso_base # if k==0 then 1
+	beq $a0, $a1, caso_base # if k==n then 1
+	
+	# CASO RECURSIVO
+	subi $a0, $a0, 1 # n-1
+	subi $a1, $a1, 1 # k-1
+	jal coef_bin #calcula recursivamente (n-1, k-1)
+	move $t0, $v0 #guarda el resultado en t0
+ 
+	lw   $a0, 4($sp) # recupera los valores iniciales
+    	lw   $a1, 8($sp) # recupera los valores iniciales
+	subi $a0, $a0, 1 # n-1
+	jal coef_bin #calcula recursivamente (n-1, k)
+	move $t1, $v0 #guarda el resultado en t1
+
+    	add $v0, $t0, $t1 # (n-1,k-1)+(n-1,k)
+    	
+    	# CONCLUSION
+    	lw   $ra, 0($sp) # recupera ra 
+    	lw   $a0, 4($sp) # recupera los valores iniciales
+    	lw   $a1, 8($sp) # recupera los valores iniciales
+	addi $sp, $sp, 12 # destruye la pila
+    	jr   $ra #return
+
+caso_base:
+	li $v0, 1 #retorna 1
+	jr $ra #regresa a donde se quedó en la ejecución
+	
+exit:
+    	li $v0, 10 #sale del programa
+    	syscall	
+
+
+
+
+
+
+
+
+ 
