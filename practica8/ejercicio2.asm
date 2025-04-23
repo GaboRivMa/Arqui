32b9f723748ce0a4091e5e489d05e5e185c0e0f4
IlseBM8 <ilse_banos@ciencias.unam.mx>
2025-04-22 22:44:59 -0600

    se termino ejercicio 2

--- practica8/ejercicio2.asm
+++ practica8/ejercicio2.asm
@@ -1,24 +1,21 @@
 	.data
-n:	.word 2
-k:	.word 1
-mensaje: .asciiz "El coeficiente binomial es : "
+n:	.word 5
+k:	.word 2
+str_a: .asciiz "El coeficiente binomial de "
+str_b: .asciiz " en "
+str_c: .asciiz " es: "
 	.text
 	
 main:
 	#prepara los valores para trabajar
 	lw 	$a0, n #guarda n
-	lw 	$a1, k #guarda 
+	lw 	$a1, k #guarda k
+	
 	#invoca a coef_bin para obtener el coeficniente binomail
 	jal coef_bin
 	move $s0, $v0  # Guardar resultado en $s0
 	
-	li   $v0, 4         # Syscall para imprimir cadena
-    	la   $a0, mensaje   # Cargar dirección del mensaje
-    	syscall
-    	move $a0, $s0 
-	li   $v0, 1         # Syscall para imprimir entero
-    	syscall
-	j exit	
+	j imprimir_resultado
 
 	
 coef_bin:
@@ -27,35 +24,73 @@ coef_bin:
 	sw   $ra, 0($sp) #guarda la direccion para volver
     	sw   $a0, 4($sp) #guarda los valores 
     	sw   $a1, 8($sp) #guarda los valores
-	
+    	
 	# CASOS BASE
 	beqz $a1, caso_base # if k==0 then 1
 	beq $a0, $a1, caso_base # if k==n then 1
 	
-	# CASO RECURSIVO
+	# CASO RECURSIVO 1
 	subi $a0, $a0, 1 # n-1
 	subi $a1, $a1, 1 # k-1
 	jal coef_bin #calcula recursivamente (n-1, k-1)
-	move $t0, $v0 #guarda el resultado en t0
- 
-	lw   $a0, 4($sp) # recupera los valores iniciales
-    	lw   $a1, 8($sp) # recupera los valores iniciales
-	subi $a0, $a0, 1 # n-1
+	add $t0, $t0, $v0 # guarda resultado
+	
+	# RECUPERA LOS VALORES DE N , K
+    	lw   $a0, 4($sp)   
+    	lw   $a1, 8($sp)   
+    	
+    	# CASO RECURSIVO 2
+ 	subi $a0, $a0, 1 # n-1
 	jal coef_bin #calcula recursivamente (n-1, k)
-	move $t1, $v0 #guarda el resultado en t1
+	add $t1, $t1, $v0 # guarda resultado
 
+	# OBTIENE EL RESULTADO DE LA RECURSION
     	add $v0, $t0, $t1 # (n-1,k-1)+(n-1,k)
     	
-    	# CONCLUSION
+    	# REINICIA LO ACUMULADO
+    	move $t0, $zero
+    	move $t1, $zero
+    	
+	# CONCLUSION
     	lw   $ra, 0($sp) # recupera ra 
-    	lw   $a0, 4($sp) # recupera los valores iniciales
-    	lw   $a1, 8($sp) # recupera los valores iniciales
 	addi $sp, $sp, 12 # destruye la pila
-    	jr   $ra #return
-
+    	jr $ra #regresa a donde se quedó en la ejecución
+    	
+    	
 caso_base:
-	li $v0, 1 #retorna 1
+	addi $v0, $zero, 1 #retorna 1
+	lw   $ra, 0($sp) # recupera ra
+       	addi $sp, $sp, 12 # destruye la pila
 	jr $ra #regresa a donde se quedó en la ejecución
+	
+	
+imprimir_resultado:
+	#	"El coeficiente binomial de"
+	li   $v0, 4         # Syscall para imprimir cadena
+    	la   $a0, str_a  # Cargar dirección del mensaje
+    	syscall
+    	lw 	$a0, n #guarda n
+	lw 	$a1, k #guarda k
+    	#	"n"
+	li   $v0, 1         # Syscall para imprimir entero
+    	syscall
+    	#	"en "
+    	li   $v0, 4         # Syscall para imprimir cadena
+    	la   $a0, str_b  # Cargar dirección del mensaje
+    	syscall
+    	#	"k"
+    	move $a0, $a1 
+	li   $v0, 1         # Syscall para imprimir entero
+    	syscall
+    	#	"es: "
+    	li   $v0, 4         # Syscall para imprimir cadena
+    	la   $a0, str_c  # Cargar dirección del mensaje
+    	syscall
+    	#	resultado
+    	move $a0, $s0 
+	li   $v0, 1         # Syscall para imprimir entero
+    	syscall
+    	
 	
 exit:
     	li $v0, 10 #sale del programa
